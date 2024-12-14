63df95c84750d93be3be0b8a02d52b1a142e9ef0
pqud <49116761+pqud@users.noreply.github.com>
2024-12-14 23:32:24 +0900

    sidebar button

--- _javascript/modules/components/sidebar.js
+++ _javascript/modules/components/sidebar.js
@@ -18,18 +18,9 @@ class SidebarUtil {
 
     SidebarUtil.isExpanded = !SidebarUtil.isExpanded;
   }
-
-  static close(){ //강제닫기
-    if(SidebarUtil.isExpanded){
-      $body.removeAttr(ATTR_DISPLAY);
-      SidebarUtil.isExpanded=false;
-      console.log($("#sidebar-button .closebtn")); // 확인용
-    }
-  }
 }
 
 export function sidebarExpand() {
   $('#sidebar-trigger').on('click', SidebarUtil.toggle); //사이드바 열기닫기 트리거
   $('#mask').on('click', SidebarUtil.toggle); //마스크 클릭시 사이드바 닫기
-  $('#sidebar-button button').on('click', SidebarUtil.close); // 닫기 버튼 클릭 시 닫기
 }
