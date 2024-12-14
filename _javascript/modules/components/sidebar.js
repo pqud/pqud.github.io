7211d938e955f8b1ebad5844a05bebfae2874134
pqud <49116761+pqud@users.noreply.github.com>
2024-12-14 22:53:54 +0900

    close button

--- _javascript/modules/components/sidebar.js
+++ _javascript/modules/components/sidebar.js
@@ -2,6 +2,7 @@
  * Expand or close the sidebar in mobile screens.
  */
 
+
 const $body = $('body');
 const ATTR_DISPLAY = 'sidebar-display';
 
@@ -10,16 +11,24 @@ class SidebarUtil {
 
   static toggle() {
     if (SidebarUtil.isExpanded === false) {
-      $body.attr(ATTR_DISPLAY, '');
+      $body.attr(ATTR_DISPLAY, ''); //사이드바 열기
     } else {
-      $body.removeAttr(ATTR_DISPLAY);
+      $body.removeAttr(ATTR_DISPLAY); //사이드바 닫기기
     }
 
     SidebarUtil.isExpanded = !SidebarUtil.isExpanded;
   }
+
+  static close(){
+    if(SidebarUtil.isExpanded){
+      $body.removeAttr(ATTR_DISPLAY);
+      SidebarUtil.isExpanded=false;
+    }
+  }
 }
 
 export function sidebarExpand() {
-  $('#sidebar-trigger').on('click', SidebarUtil.toggle);
-  $('#mask').on('click', SidebarUtil.toggle);
+  $('#sidebar-trigger').on('click', SidebarUtil.toggle); //사이드바 열기닫기 트리거
+  $('#mask').on('click', SidebarUtil.toggle); //마스크 클릭시 사이드바 닫기
+  $("#sidebar-button .closebtn").on('click',SidebarUtil.close); //버튼 클릭시 사이드바 닫기
 }
